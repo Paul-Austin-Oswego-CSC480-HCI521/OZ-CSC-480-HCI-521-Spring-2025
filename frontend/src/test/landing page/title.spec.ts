8c6997f6c8daf85036e290421a4af816ff98f4e0
Praneeta Pradhan <ppradhan@oswego.edu>
2025-04-25 10:24:01 -0400

    test

--- frontend/src/test/landing page/title.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.describe("Home Page",()=>{
-    test("Should have correct title",async ({page})=>{
-        await expect(page).toHaveTitle("Quotable");
-    });
-});
